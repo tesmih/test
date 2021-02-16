a7e8e7e0336b48893cec8b8fbb372fe9ed0ff08e
Mike <teslenko2603@gmail.com>
2021-02-16 16:34:56 +0200

    Первый комит

---
+++ script.js
@@ -0,0 +1,15 @@
+
+var leftBorder = 1;
+let second = 2;
+const pi = 3.14;
+
+var person = {
+    name: "Nick",
+    age: 30,
+    married: false
+};
+console.log(person.name);
+var itsClose = true,
+    itsCheck = false;
+
+console.log(itsCheck || !itsClose)
